1b67e03546a3daa064210a63d5282fa2ea01e9df
Michael_Scopic <67214805+michaelScopic@users.noreply.github.com>
2024-03-06 02:56:13 -0600

    Initial commit
    (technically not the initial commit, just a few grammatical corrections in the comments)

--- purge-discord-dms.py
+++ purge-discord-dms.py
@@ -4,16 +4,17 @@
 import pyautogui
 import time
 
-# Just setted this so that I don't gotta type as much 
+# Just set this so that I don't gotta type as much 
 pag = pyautogui
 
-# Moving ur cursor to the top left of ur screen aborts this script
+# Moving your cursor to the top left of the screen aborts this script
 pag.FAILSAFE = True
 
 # Wait 5 seconds
 pag.alert('STARTING IN 5 SECONDS!')
 time.sleep(5)
 
+### MAIN FUNCTION ###
 # Will go on forever until the script is quit
 while True:
     # Press the up arrow key
